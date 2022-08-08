8734981dc83323c80c7941ce622406c9bbe59fe7
Thuan123abc <phuvanthuan@gmail.com>
2022-08-08 23:46:56 +0700

    Initial commit

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package hotel
 
-func main() {
+import "fmt"
 
+func main() {
+	cus1 := Customer{
+		Name:        "Thuan",
+		Age:         25,
+		IdentityNum: 2343442,
+		TimeRent:    4,
+		Skind:       "A",
+	}
+	fmt.Printf("so tien cu khac phai tra la:%v\n", cus1.Money())
 }
